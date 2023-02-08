c64e980a51b780a23ef36a48f8c7a13b20126fc7
juan ricaud <ricaudjuan@hotmail.com>
2023-02-08 05:33:43 -0300

    done

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser')
+const dns = require('node:dns');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+const shortUrl = [];
+let id = 0;
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -18,6 +23,35 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.post("/api/shorturl", (req, res) => {
+  const { url } = req.body;
+  console.log(url)
+  const checkUrl = url.replace(/^https?:\/\//, "")
+  console.log('check', checkUrl)
+  dns.lookup(checkUrl, (err) => {
+    if(err){
+      return res.json({ error : "invalid url"})
+    }
+    ++id;
+    const newUrl = {
+      original_url : url,
+      short_url : id
+    }
+    shortUrl.push(newUrl);
+
+    return res.json(newUrl)
+  })
+})
+
+app.get("/api/shorturl/:url", (req, res) => {
+  const { url } = req.params;
+  
+  const urlRedirect = shortUrl.find(el => el.short_url == url);
+  
+  return res.redirect(urlRedirect.original_url)
+  
+} )
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
